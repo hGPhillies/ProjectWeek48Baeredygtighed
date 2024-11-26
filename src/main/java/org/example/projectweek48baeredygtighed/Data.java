15af355ea141c4cf15498fe07a62682687e79077
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2024-11-26 10:19:37 +0100

    Data class

---
+++ src/main/java/org/example/projectweek48baeredygtighed/Data.java
@@ -0,0 +1,71 @@
+package org.example.projectweek48baeredygtighed;
+
+import java.util.Date;
+
+public class Data {
+
+    private int id;
+    private int siteId;
+    private int total;
+    private int online;
+    private int offline;
+    private Date date;
+    private int hour;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(int siteId) {
+        this.siteId = siteId;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getOnline() {
+        return online;
+    }
+
+    public void setOnline(int online) {
+        this.online = online;
+    }
+
+    public int getOffline() {
+        return offline;
+    }
+
+    public void setOffline(int offline) {
+        this.offline = offline;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+}
