a1faee04bd5e14f4bb4c97e3c8e07fca26f276bc
Krull <Sebastian-krull@hotmail.com>
2024-11-28 11:13:50 +0100

    Up scaling of UI

--- src/main/java/org/example/projectweek48baeredygtighed/HelloApplication.java
+++ src/main/java/org/example/projectweek48baeredygtighed/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 448);
+        Scene scene = new Scene(fxmlLoader.load(), 872.0, 1195.0);
         stage.setTitle("Solar energy");
         stage.setScene(scene);
         stage.show();
